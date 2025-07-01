b4af41c9471a8491f4f4bef1a2b92e70bb9f5213
Yimei Liang <sophia@YimeideMacBook-Air.local>
2025-06-30 21:58:57 -0700

    first part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from './routes/user.routes.js'
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+const app = express();
+dotenv.config(); // 自动读取根目录下.env文件，把里面的内容加载进process.env
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (req.body)
+app.use(cookieParser());
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
+
+
+
+// app.get("/", (req, res) => {
+//     // root route http://localhost:3000/
+//     res.send("Hello World！！！!!")
+// })
+
+app.listen(PORT, () => {
+
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)  })
