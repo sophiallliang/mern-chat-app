cbd62645d5cb035ac365107c442584c801b79e12
Yimei Liang <sophia@YimeideMacBook-Air.local>
2025-07-03 15:00:14 -0700

    improve checkbox spacing and checkmark visibility for gender selection

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,16 +1,18 @@
 const GenderCheckbox = () => {
 	return (
-		<div className='flex p-2 gap-3'>
+		<div className='flex p-2 gap-3 my-2'>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer`}>
 					<span className='label-text'>Male</span>
-					<input type='checkbox' className='checkbox border-slate-900' />
+					<input type='checkbox' className='checkbox checkbox-primary border-slate-900' />
+
 				</label>
 			</div>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer`}>
 					<span className='label-text'>Female</span>
-					<input type='checkbox' className='checkbox border-slate-900' />
+					<input type='checkbox' className='checkbox checkbox-primary border-slate-900' />
+
 				</label>
 			</div>
 		</div>
